c1d2152047cdabb41b88f698a458334f6800dbac
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-12-20 15:43:28 +0000

    start working on ui placement

--- src/components/tabs/Navigation.js
+++ src/components/tabs/Navigation.js
@@ -19,18 +19,28 @@ const fullStop = () => {
 const Navigation = () => {
     return (
         <div className="NavigationTab-wrapper">
-            <div>
-                <button onClick={() => rotate(225)}>Rotate 225</button>
-                <button onClick={() => rotate(270)}>Rotate 270</button>
-                <button onClick={() => rotate(315)}>Rotate 315</button>
-                <button onClick={() => rotate(0)}>Rotate 0</button>
-                <button onClick={() => rotate(45)}>Rotate 45</button>
-                <button onClick={() => rotate(90)}>Rotate 90</button>
-                <button onClick={() => rotate(180)}>Rotate 180</button>
+            <div className="NavigationTab-top">
+                <div className="NavigationTab-top-row">
+                    <div className="NavigationTab-heading">
+                        Speed:
+                    </div>
+                    <div className="NavigationTab-heading">
+                        <button onClick={() => rotate(225)}>Rotate 225</button>
+                        <button onClick={() => rotate(270)}>Rotate 270</button>
+                        <button onClick={() => rotate(315)}>Rotate 315</button>
+                        <button onClick={() => rotate(0)}>Rotate 0</button>
+                        <button onClick={() => rotate(45)}>Rotate 45</button>
+                        <button onClick={() => rotate(90)}>Rotate 90</button>
+                        <button onClick={() => rotate(180)}>Rotate 180</button>
+                    </div>
+                </div>
+                <div className="NavigationTab-engine">
+                    <button onClick={burn}>Burn 2 sec</button>
+                    <button onClick={fullStop}>Full Stop</button>
+                </div>
             </div>
-            <div>
-                <button onClick={burn}>Burn 2 sec</button>
-                <button onClick={fullStop}>Full Stop</button>
+            <div className="NavigationTab-bottom">
+                Fuel
             </div>
         </div>
     );
