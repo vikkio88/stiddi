b84485fe3d93e5a7ecb99cddf951e0dad2d738e5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-12-20 09:16:56 +0000

    moved tests alongside code

--- src/libs/math.js
+++ src/libs/math.js
@@ -10,18 +10,6 @@ export const ANGLES = {
     DEG_360: 360,
 };
 
-const NEGATIVE_ANGLES = {
-    [ANGLES.DEG_0]: 0,
-    [ANGLES.DEG_45]: -315,
-    [ANGLES.DEG_90]: -270,
-    [ANGLES.DEG_135]: -225,
-    [ANGLES.DEG_180]: -180,
-    [ANGLES.DEG_225]: -135,
-    [ANGLES.DEG_270]: -90,
-    [ANGLES.DEG_315]: -45,
-    [ANGLES.DEG_360]: -360,
-};
-
 export const Angle = {
     shortestRotation(currentAngle, wantedAngle) {
         const currentNegative = currentAngle - ANGLES.DEG_360;
@@ -33,10 +21,6 @@ export const Angle = {
             Math.abs(currentNegative - negativeAngle) :
             ANGLES.DEG_360 + currentAngle - wantedAngle;
         return {
-            /*
-            pos: positiveRotation,
-            neg: negativeRotation,
-            */
             rotation: (positiveRotation <= negativeRotation ? positiveRotation : negativeRotation) % ANGLES.DEG_360,
             angle: positiveRotation <= negativeRotation ? wantedAngle : negativeAngle
         };
