874e63f862797549cedee817185669b594c4ad9b
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-01-01 13:52:21 +0000

    made slider more customisable

--- src/components/common/Slider.js
+++ src/components/common/Slider.js
@@ -1,9 +1,10 @@
 import "./styles/Slider.css";
 
-const Slider = ({ min = 1, max = 10, step = 1, value = 1, onChange = () => { } }) => {
+const Slider = ({ min = 1, max = 10, step = 1, value = 1, onChange = () => { }, style = {}, className = '' }) => {
     return (
         <input
-            className="slider"
+            style={style}
+            className={`slider ${className}`}
             type="range"
             min={min}
             max={max}
