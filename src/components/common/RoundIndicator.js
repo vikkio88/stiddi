deacad3bef0893a011254d86f5231315cd8a5134
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-01-01 20:05:24 +0000

    small refactory on shared props

--- src/components/common/RoundIndicator.js
+++ src/components/common/RoundIndicator.js
@@ -6,6 +6,14 @@ const RoundIndicator = ({ radius = 50, stroke = 5, max = 100, value = 10, colour
     const normalizedRadius = radius - stroke * 2;
     const circumference = normalizedRadius * 2 * Math.PI;
     const strokeDashoffset = circumference - ((value / max) * circumference);
+    const circleProps = {
+        fill: "transparent",
+        strokeWidth: stroke,
+        strokeDasharray: `${circumference} ${circumference}`,
+        r: normalizedRadius,
+        cx,
+        cy
+    };
     return (
         <svg
             style={style}
@@ -15,23 +23,13 @@ const RoundIndicator = ({ radius = 50, stroke = 5, max = 100, value = 10, colour
             {showRail && (
                 <circle
                     stroke={railColour}
-                    fill="transparent"
-                    strokeWidth={stroke}
-                    strokeDasharray={circumference + ' ' + circumference}
-                    r={normalizedRadius}
-                    cx={radius}
-                    cy={radius}
+                    {...circleProps}
                 />
             )}
             <circle
                 stroke={colour}
-                fill="transparent"
-                strokeWidth={stroke}
-                strokeDasharray={circumference + ' ' + circumference}
                 style={{ strokeDashoffset }}
-                r={normalizedRadius}
-                cx={radius}
-                cy={radius}
+                {...circleProps}
             />
             <text textAnchor="middle" x="50%" y="50%" dy={children ? ".1em" : ".3em"} style={{ fill: "white" }}>{value}</text>
             {children && <text textAnchor="middle" x="50%" y="50%" dy="1.3em" style={{ fill: "white" }}>{children}</text>}
