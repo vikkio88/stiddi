ab1730e674475ba05ef1772335f7a73b8963c201
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-01-16 14:55:39 +0000

    small correction on orbit offset

--- src/phaser/entities/system/Planet.js
+++ src/phaser/entities/system/Planet.js
@@ -6,16 +6,15 @@ class Planet extends SystemObject {
     add({ offset, name, type, radius, colour, id, index, angle = 0 }) {
         // maybe body can be a enum
         this.setInfo({ id, name, type, body: 'planet', index, offset, radius });
-
-
         const { cx, cy } = this.getSceneCenter();
 
-        const orbitShape = new Phaser.Geom.Circle(cx, cy, offset);
+        const { x, y } = Geom.pointOnCircumference({ cx, cy }, offset, angle);
+        const orbitOffset = Phaser.Math.Distance.Between(x, y, cx, cy);
+        const orbitShape = new Phaser.Geom.Circle(cx, cy, orbitOffset);
         const orbit = this.scene.add.graphics();
         orbit.lineStyle(1, 0xffffff, .3);
         orbit.strokeCircleShape(orbitShape);
 
-        const { x, y } = Geom.pointOnCircumference({ cx, cy }, offset, angle);
         const planetShape = new Phaser.Geom.Circle(x, y, radius);
         const planet = this.scene.add.graphics();
         planet.fillStyle(colour, 2);
