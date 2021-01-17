9ca68f7da79b4da3887094a0b8b15b57c6d3a32f
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-01-17 13:59:05 +0000

    small ui tweaks

--- src/components/maps/system/Bodies.js
+++ src/components/maps/system/Bodies.js
@@ -1,42 +1,13 @@
 import { useState } from "react";
 import { useStoreon } from "storeon/react";
-import { hashHex } from "libs/colours";
+
 import { BODY_TYPES } from "enums/systemMap";
+import Row from "./Row";
 import BodyInfo from "./BodyInfo";
-import { Button, Circle, Star } from "components/common";
+import { Button } from "components/common";
 
 import "./styles/Bodies.css";
 
-const Row = ({ index, name, colour, offset = 0, body, showInfo }) => {
-    const hashedCoulour = hashHex(colour);
-    const isPlanet = body === BODY_TYPES.PLANET;
-    return (
-        <div className="Bodies-Row">
-            <div className="f-1 ml-5 flex f-jsa f-ac">
-                {`#${index + 1 + (isPlanet ? 1 : 0)}`}
-                {isPlanet ? <Circle radius={15} colour={hashedCoulour} /> : <Star size={30} colour={hashedCoulour} />}
-            </div>
-
-            <div className="f-3 ml-5">
-                {`${name}`}
-            </div>
-
-            <div className="f-1 flex f-jc f-ac">
-                {offset > 0 ? `${offset} Ls` : '-'}
-            </div>
-
-            <div className="f-2 flex f-je f-ac pr-5">
-                <Button
-                    style={{ height: "30px" }}
-                    onClick={() => showInfo({ object: body, index })}
-                >
-                    Info
-                </Button>
-            </div>
-
-        </div>
-    );
-};
 
 const Bodies = ({ system = {}, onFocus, onPlot, onLock }) => {
     const { dispatch, player: { position } } = useStoreon('player');
@@ -48,6 +19,7 @@ const Bodies = ({ system = {}, onFocus, onPlot, onLock }) => {
         setSelected(body);
     };
     const bodiesCount = system.stars.length + system.planets.length;
+    const selectedId = `${selected.object}${selected.index}`;
     return (
         <>
             <div className="ui-section p-5 mt-10 mb-5 flex f-col">
@@ -72,10 +44,22 @@ const Bodies = ({ system = {}, onFocus, onPlot, onLock }) => {
             </div>
             <div className={`Bodies-List${bodiesCount > 7 ? ' List-overflowing' : ''}`}>
                 {system.stars.map((s, i) => (
-                    <Row key={`star_${i}`} {...s} body={BODY_TYPES.STAR} showInfo={select} />
+                    <Row
+                        key={`star_${i}`}
+                        {...s}
+                        body={BODY_TYPES.STAR}
+                        showInfo={select}
+                        isSelected={selectedId === `${BODY_TYPES.STAR}${i}`}
+                    />
                 ))}
                 {system.planets.map((p, i) => (
-                    <Row key={`planet_${i}`} {...p} body={BODY_TYPES.PLANET} showInfo={select} />
+                    <Row
+                        key={`planet_${i}`}
+                        {...p}
+                        body={BODY_TYPES.PLANET}
+                        showInfo={select}
+                        isSelected={selectedId === `${BODY_TYPES.PLANET}${i}`}
+                    />
                 ))}
             </div>
         </>
---
+++ src/components/maps/system/Row.js
@@ -0,0 +1,36 @@
+import { hashHex } from "libs/colours";
+import { BODY_TYPES } from "enums/systemMap";
+import { Button, Circle, Star } from "components/common";
+
+const Row = ({ index, name, colour, offset = 0, body, showInfo, isSelected = false }) => {
+    const hashedCoulour = hashHex(colour);
+    const isPlanet = body === BODY_TYPES.PLANET;
+    return (
+        <div className={`Bodies-Row${isSelected ? ' Bodies-Row-selected' : ''}`}>
+            <div className="f-1 ml-5 flex f-jsa f-ac">
+                {`#${index + 1 + (isPlanet ? 1 : 0)}`}
+                {isPlanet ? <Circle radius={15} colour={hashedCoulour} /> : <Star size={30} colour={hashedCoulour} />}
+            </div>
+
+            <div className="f-3 ml-5">
+                {`${name}`}
+            </div>
+
+            <div className="f-1 flex f-jc f-ac">
+                {offset > 0 ? `${offset} Ls` : '-'}
+            </div>
+
+            <div className="f-2 flex f-je f-ac pr-5">
+                <Button
+                    style={{ height: "30px" }}
+                    onClick={() => showInfo({ object: body, index })}
+                >
+                    Info
+                </Button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Row;
